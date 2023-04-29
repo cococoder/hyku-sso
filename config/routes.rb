ef7453823a76811b8cae3de0a3aa50453fc4dee3
The Coco Coder <shout@cococoder.com>
2023-04-29 08:50:33 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 Hyrax::Sso::Engine.routes.draw do
-  
+  get "/sso/login", to: "controller#auth", as: :sso_login
+  get "/sso/callback", to: "controller#callback", as: :sso_callback
 end
