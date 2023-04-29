c64f3ab0aa66bbab568ad989ee0b672cadb8ec3a
Delaney Burke <deaney.burke@ubiquitypress.com>
2023-04-29 08:44:42 +0300

    add the controller

--- app/controllers/hyku/sso/controller.rb
+++ app/controllers/hyku/sso/controller.rb
@@ -2,46 +2,46 @@ module Hyku
   module Sso
     class Controller < ::Hyku::API::V1::SessionsController
 
-    before_action :set_account, only: :callback
+      before_action :set_account, only: :callback
 
-    def auth
-      redirect_to HykuAddons::Sso::AuthService.new(host: request.host).generate_authorisation_url
-    end
-
-    def callback
-      service = HykuAddons::Sso::CallBackService.new(code: params[:code])
-
-      handled = false
-
-      service.handle do |profile, password|
-        
-        user = User.find_or_create_by(email: profile.email).tap do |u|
-          u.password = password
-          u.password_confirmation = password
-          u.email = profile.email
-        end
-        # this code is the same as the code used in the api for authentication
-        user = User.find_for_database_authentication(email: user.email)
-        sign_in user
-        set_jwt_cookies(user)
-        handled = true
-
+      def auth
+        redirect_to HykuAddons::Sso::AuthService.new(host: request.host).generate_authorisation_url
       end
 
-      raise HykuAddons::Sso::Error, "Failed to handle workos code #{params[:code]}" unless handled
+      def callback
+        service = HykuAddons::Sso::CallBackService.new(code: params[:code])
 
-      redirect_to "/dashboard"
-      # Use the information in `profile` for further business logic.
+        handled = false
+
+        service.handle do |profile, password|
+
+          user = User.find_or_create_by(email: profile.email).tap do |u|
+            u.password = password
+            u.password_confirmation = password
+            u.email = profile.email
+          end
+          # this code is the same as the code used in the api for authentication
+          user = User.find_for_database_authentication(email: user.email)
+          sign_in user
+          set_jwt_cookies(user)
+          handled = true
+
+        end
+
+        raise HykuAddons::Sso::Error, "Failed to handle workos code #{params[:code]}" unless handled
+
+        redirect_to "/dashboard"
+        # Use the information in `profile` for further business logic.
+      end
+
+      def current_account
+        @account ||= Account.find_by cname: request.env["SERVER_NAME"]
+      end
+
+      def set_account
+        AccountElevator.switch!(current_account.cname) if current_account.present?
+      end
+
     end
-
-    def current_account
-      @account ||= Account.find_by cname: request.env["SERVER_NAME"]
-    end
-
-    def set_account
-      AccountElevator.switch!(current_account.cname) if current_account.present?
-    end
-
-  end
   end
 end
